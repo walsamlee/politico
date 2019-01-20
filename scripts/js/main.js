93dbe957425e65e6864c2d5194a0bd826f2098d4
Akinwale Samuel <wale0331@gmail.com>
2019-01-20 19:25:00 +0100

    Refactor branch

--- scripts/js/main.js
+++
@@ -1,8 +0,0 @@
-const miniMenu = () => {
-    const x = document.getElementById("menu");
-    if (x.className === "menu") {
-      x.className += " responsive";
-    } else {
-      x.className = "menu";
-    }
-  }
