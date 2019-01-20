942efb8c3d2f707d1303b349f342435457aefb36
Akinwale Samuel <wale0331@gmail.com>
2019-01-20 19:29:03 +0100

    Modify .gitignore

---
+++ ui/scripts/js/main.js
@@ -0,0 +1,8 @@
+const miniMenu = () => {
+    const x = document.getElementById("menu");
+    if (x.className === "menu") {
+      x.className += " responsive";
+    } else {
+      x.className = "menu";
+    }
+  }
