8d86a9ce3c62f7fbf0e6e41ae7575f0556e37112
Akinwale Samuel <wale0331@gmail.com>
2019-01-22 11:18:47 +0100

    Chore(setup): setup linting library

--- api/app.js
+++ api/app.js
@@ -5,25 +5,25 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
+  console.log(`App listening on port ${port}`);
 });
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-    const err = new Error('Not found');
-    err.status = 404;
+  const err = new Error('Not found');
+  err.status = 404;
   next(err);
 });
 
 // error handler
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   // render the error page
   res.status(err.status || 500);
   res.json({
-      message: err.message,
-      error: req.app.get('env') === 'development' ? err : {
-          status: 404
-      }
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err : {
+      status: 404,
+    },
   });
 });
 
