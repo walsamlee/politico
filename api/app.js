af872e43fbb5b16d0ada5afb4455a0c463cf2d66
Akinwale Samuel <wale0331@gmail.com>
2019-01-22 10:52:44 +0100

    Chore(setup): server setup
    - install express module
    - install babel-cli, babel-preset-env and babel-polyfill for transpiling

---
+++ api/app.js
@@ -0,0 +1,30 @@
+import express from 'express';
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    const err = new Error('Not found');
+    err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // render the error page
+  res.status(err.status || 500);
+  res.json({
+      message: err.message,
+      error: req.app.get('env') === 'development' ? err : {
+          status: 404
+      }
+  });
+});
+
+module.exports = server;
