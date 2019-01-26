322db02ea672b350535ac1d53ae90efa5161f54f
Akinwale Samuel <wale0331@gmail.com>
2019-01-26 11:42:57 +0100

    Refactor(endpoint): add models

--- server/v1/controllers/Parties.js
+++ server/v1/controllers/Parties.js
@@ -1,51 +1,26 @@
-const allParties = [
-    {
-        id: 1,
-        name: 'Action Peoples Congress',
-        hqAddress: 'Wuse Zone II, Abuja, Nigeria',
-        logoUrl: '../assets/images/apc.png'
-    },
-    {
-        id: 2,
-        name: 'All Democratic Congress',
-        hqAddress: 'Wuse Zone I, Abuja, Nigeria',
-        logoUrl: '../assets/images/adc.png'
-    },
-    {
-        id: 3,
-        name: 'Alliance for Democracy Party',
-        hqAddress: 'Gwarinpa, Abuja, Nigeria',
-        logoUrl: '../assets/images/adp.jpg'
-    },
-    {
-        id: 4,
-        name: 'All Peoples Grand Alliance',
-        hqAddress: 'Garki, Abuja, Nigeria',
-        logoUrl: '../assets/images/adc.png'
-    }
-];
+import Db from '../models/db';
 
 const Parties = {
     viewPartyById(req, res, next) {
         const partyId = parseInt(req.params.partyId);
+        
+        const party = Db.viewParty(partyId);
 
-        allParties.forEach(party => {
-            if(party.id === partyId) {
-                return res.json({
-                    status: 200,
-                    data: {
-                        id: party.id,
-                        name: party.name,
-                        logoUrl: party.logoUrl
-                    }
-                });              
+        if(party.length === 1) {
+          return res.json({
+            status: 200,
+            data: {
+              id: partyId,
+              name: party[0].name,
+              logoUrl: party[0].logoUrl
             }
-        });
-
-        res.json({
-            status: 404,
-            error: `Party with ID ${partyId} not found`
-        });
+          })
+        } else {
+            return res.json({
+              status: 404,
+              error: `Party with ID ${partyId} not found`
+          });
+        }        
       }
 };
 
