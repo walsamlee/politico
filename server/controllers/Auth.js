b4f1dbdbedbbcc7cba101bc51e8e6ba16f7e0064
Akinwale Samuel <wale0331@gmail.com>
2019-01-30 17:12:56 +0100

    Refactor(chore): update line indentation

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -2,45 +2,45 @@ import db from '../models/db';
 import Validations from './Validations';
 
 const signup = (req, res) => {
-    const result = Validations.validateUser(req.body);
+  const result = Validations.validateUser(req.body);
 
-    if(result.error) {
-        return res.json({
-            status: 400,
-            error: `${result.error.details[0].context.value} is an invalid value`,
-          });
+  if (result.error) {
+    return res.json({
+      status: 400,
+      error: `${result.error.details[0].context.value} is an invalid value`,
+    });
+  }
+
+  const query = {
+    text: 'INSERT INTO users(email, password, firstname, lastname) VALUES($1, $2, $3, $4)',
+    values: [req.body.email, req.body.password, req.body.firstName, req.body.lastName],
+  };
+
+  db.client.query(query, (err, result) => {
+    if (err) {
+      return res.json({
+        status: 400,
+        message: 'Data cannot be added to database',
+      });
     }
-
-    const query = {
-        text: 'INSERT INTO users(email, password, firstname, lastname) VALUES($1, $2, $3, $4)',
-        values: [req.body.email, req.body.password, req.body.firstName, req.body.lastName]
-    }
-
-    db.client.query(query, (err, result) => {
-        if(err) {
-            return res.json({
-                status: 400,
-                message: 'Data cannot be added to database',
-              });
-        }
-        return res.json({
-            status: 200,
-            data: [
-                {
-                    token: 0,
-                    user: {
-                        firstName: req.body.firstName,
-                        lastName: req.body.lastName,
-                        email: req.body.email,
-                    }
-                }
-            ]
-        });
-    })
-}
+    return res.json({
+      status: 200,
+      data: [
+        {
+          token: 0,
+          user: {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+          },
+        },
+      ],
+    });
+  });
+};
 
 const Auth = {
-    signup,
-}
+  signup,
+};
 
 export default Auth;
