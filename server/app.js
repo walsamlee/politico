4e54497abf47a4ab0f258baf250459a6641a5659
Akinwale Samuel <wale0331@gmail.com>
2019-01-31 04:37:32 +0100

    Feat(endpoint): add votes endpoint

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 
 import authRouter from './routes/authRoute';
 import router from './routes/router';
+import voteRouter from './routes/voteRouter';
 
 const app = express();
 
@@ -13,6 +14,7 @@ const port = process.env.PORT || 3000;
 
 app.use('/auth', authRouter);
 app.use('/api/v1', router);
+app.use('/votes', voteRouter);
 
 const server = app.listen(port, () => {
   console.log(`App listening on port ${port}`);
