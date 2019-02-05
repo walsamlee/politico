8ea860d60b9d57bb39aaf66b39b357021de6b732
Akinwale Samuel <wale0331@gmail.com>
2019-02-05 23:38:36 +0100

    Refactor(chore): eslint fix

--- templates/scripts/js/adminjs/login.js
+++ templates/scripts/js/adminjs/login.js
@@ -1,30 +1,27 @@
 const thisLogin = (form) => {
-    console.log(form.email.value);
-    console.log(form.password.value);
-    const data = {
-        email: form.email.value,
-        password: form.password.value
-    };
+  const data = {
+    email: form.email.value,
+    password: form.password.value,
+  };
 
-    const url = 'https://sam-politico.herokuapp.com/api/v1/auth/login';
-    
-    fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
+  const url = 'https://sam-politico.herokuapp.com/api/v1/auth/login';
+
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
     .then(res => res.json())
-    .then(response => {
-        if(response.message) {
-            alert('Invalid Username or password!');
-        } else {
-            console.log(response.data[0].token);
-            sessionStorage.setItem("token", response.data[0].token);
-            window.location = 'http://' + document.location.host + '/index.html';
-        }
-        
+    .then((response) => {
+      if (response.error) {
+        document.getElementById('failure').style.display = 'block';
+        setTimeout(() => { document.getElementById('failure').style.display = 'none'; }, 3000);
+      } else {
+        sessionStorage.setItem('token', response.data[0].token);
+        window.location = `http://${document.location.host}/index.html`;
+      }
     })
     .catch(error => console.error('Error:', error));
 };
