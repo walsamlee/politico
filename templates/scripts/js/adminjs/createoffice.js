8cf3f82daafbe72b8323b58b44d40bad9016c990
Akinwale Samuel <wale0331@gmail.com>
2019-02-05 23:08:04 +0100

    Chore(offices): add create office function

---
+++ templates/scripts/js/adminjs/createoffice.js
@@ -0,0 +1,36 @@
+const createOfficeToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZW1haWwiOiJ1c2VyM0Bwb2xpdGljby5jb20iLCJwcml2aWxlZ2UiOjEsImlhdCI6MTU0OTQwMTY2NCwiZXhwIjoxNTgwOTU5MjY0fQ.0l8bspMrUvO9bYB3koEG6qSP0xtwni3xOJ245S0qBPo';
+
+const createOffice = (form) => {
+    const url = 'https://sam-politico.herokuapp.com/api/v1/offices';
+    const officeId = form.officeid.value;
+    const officeType = form.officetype.value;
+    const officeName = form.officename.value;
+
+    const officeData = {
+        id: officeId,
+        type: officeType,
+        name: officeName,
+    }
+    console.log(createOfficeToken);
+    console.log(officeData);
+    
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(officeData),
+        headers: {
+            'Content-Type': 'application/json',
+            token: createOfficeToken,
+        }
+    })
+    .then(res => res.json())
+    .then(response => {
+        console.log(response);
+        if(response.status === 201) {
+            document.getElementById('success').style.display = 'block';
+            setTimeout(function(){ document.getElementById('success').style.display = 'none'; }, 3000);
+        }
+    })
+    .catch(error => console.log(error));
+
+    
+}
