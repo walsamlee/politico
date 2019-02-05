b400643c214c45d7205709aeb31496e2948e56a2
Akinwale Samuel <wale0331@gmail.com>
2019-02-05 23:16:18 +0100

    Refactor(office): eslint fix

--- templates/scripts/js/adminjs/createoffice.js
+++ templates/scripts/js/adminjs/createoffice.js
@@ -1,36 +1,31 @@
 const createOfficeToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZW1haWwiOiJ1c2VyM0Bwb2xpdGljby5jb20iLCJwcml2aWxlZ2UiOjEsImlhdCI6MTU0OTQwMTY2NCwiZXhwIjoxNTgwOTU5MjY0fQ.0l8bspMrUvO9bYB3koEG6qSP0xtwni3xOJ245S0qBPo';
 
 const createOffice = (form) => {
-    const url = 'https://sam-politico.herokuapp.com/api/v1/offices';
-    const officeId = form.officeid.value;
-    const officeType = form.officetype.value;
-    const officeName = form.officename.value;
+  const url = 'https://sam-politico.herokuapp.com/api/v1/offices';
+  const officeId = form.officeid.value;
+  const officeType = form.officetype.value;
+  const officeName = form.officename.value;
 
-    const officeData = {
-        id: officeId,
-        type: officeType,
-        name: officeName,
-    }
-    console.log(createOfficeToken);
-    console.log(officeData);
-    
-    fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(officeData),
-        headers: {
-            'Content-Type': 'application/json',
-            token: createOfficeToken,
-        }
-    })
+  const officeData = {
+    id: officeId,
+    type: officeType,
+    name: officeName,
+  };
+
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(officeData),
+    headers: {
+      'Content-Type': 'application/json',
+      token: createOfficeToken,
+    },
+  })
     .then(res => res.json())
-    .then(response => {
-        console.log(response);
-        if(response.status === 201) {
-            document.getElementById('success').style.display = 'block';
-            setTimeout(function(){ document.getElementById('success').style.display = 'none'; }, 3000);
-        }
+    .then((response) => {
+      if (response.status === 201) {
+        document.getElementById('success').style.display = 'block';
+        setTimeout(() => { document.getElementById('success').style.display = 'none'; }, 3000);
+      }
     })
-    .catch(error => console.log(error));
-
-    
-}
+    .catch(error => console.error('Error:', error));
+};
